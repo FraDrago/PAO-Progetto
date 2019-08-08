14b4a620a46d7eb1534a95657c6aa0ab288856c8
Drago Francesco <francydrake97@gmail.com>
2019-08-08 18:47:03 +0200

    prova container

--- untitled/Gerarchia/qontainer.h
+++ untitled/Gerarchia/qontainer.h
@@ -3,13 +3,30 @@
 
 template <class T>
 class qontainer{
+    friend class const_iterator;
 private:
     class nodo{
     public:
-        Nodo* next;
+        nodo* prev;
+        nodo* next;
         T info;
-        nodo()
-    }
-}
+        nodo(nodo* =nullptr, nodo* =nullptr, T =T());
+        ~nodo()=default;
+    };
+    nodo* first;
+    nodo* last;
+    unsigned int size;
+    // unsigned int minuti;
+public:
+    qontainer(nodo* =nullptr, nodo* =nullptr, unsigned int =0);
+    qontainer(const qontainer&);
+    ~qontainer();
+
+    void insert(const T);
+    void remove(const T); //si rimuove un T costante? CONTROLLARE IN SEGUITO
+
+};
+
+
 
 #endif // QONTAINER_H
