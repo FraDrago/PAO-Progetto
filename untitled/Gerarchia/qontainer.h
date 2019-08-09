b80a0942928489287beb3bd7cf63b5895841f9bb
Drago Francesco <francydrake97@gmail.com>
2019-08-09 11:08:33 +0200

    aggiunti metodi

--- untitled/Gerarchia/qontainer.h
+++ untitled/Gerarchia/qontainer.h
@@ -25,8 +25,53 @@ public:
     void insert(const T);
     void remove(const T); //si rimuove un T costante? CONTROLLARE IN SEGUITO
 
+    unsigned int totale() const;
+
 };
 
+// NODO
+template <typename T>
+qontainer<T>::nodo::nodo(nodo* p, nodo* n, T i): prev(p), next(n), info(i) {}
+
+//qontainer
+template <typename T>
+qontainer<T>::qontainer(nodo* f, nodo* l, unsigned int s): first(f), last(l), size(s) {}
+
+template <typename T>
+qontainer<T>::qontainer(const qontainer& q): first(q.first), last(q.last), size(q.size) {}
+
+template <typename T>
+void qontainer<T>::insert(const T i){  //credo si possa fare meglio, RIGUARDARE
+    if (!first)
+        first=last=new nodo(i, nullptr, nullptr);
+    else {
+        nodo* aux=first;
+        first=last=new nodo(i, nullptr, aux);
+    }
+}
+
+template <typename T>
+void qontainer<T>::remove(const T i){
+    nodo* aux=first;
+    while (aux && !(aux->info==i)) //finchè esiste aux e le info sono diverse va avanti
+        aux=aux->next;
+    //devo dividere in diversi casi: 1. se next non c'è quindi è alla fine 2: se prev non c'è quindi è all'inizio 3: se è in mezzo quindi prev e next ci sono
+    if (aux)
+    {
+        if (!(aux->next))
+            last=last->prev;
+
+        if (!(aux->prev))
+            first=first->next;
+
+        if (aux->next && aux->prev)
+        {
+            aux->prev->next=aux->next;
+
+        }
+        delete aux;
+    }
+}
 
 
 #endif // QONTAINER_H
